cb420f0a57a5141bf3a8a8ef24714fd9200cf242
MarkusRaven <ilnaz.gimazetdinov2002@mail.ru>
2022-08-16 10:29:15 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function requireHTTPS(req, res, next) {
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+const PORT = process.env.PORT || 8080;
+const express = require('express');
+const app = express();
+app.use(express.static('./dist/my-task-frontend'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: './dist/my-task-frontend/' });
+});
+app.use(requireHTTPS);
+app.listen(PORT, () => {
+  console.log('Listening on port: ' + PORT);
+});
